6e6b99c51db6487e77897d94b061de40e1da3160
Rohit Upadhya <rohitupadhya18@gmail.com>
2025-01-04 00:09:31 +0100

    update : updated the encoder

--- src/encoder.py
+++ src/encoder.py
@@ -1,8 +1,28 @@
-from transformers import ModernBertModel, ModernBertConfig
+from typing import Text
+from transformers import AutoTokenizer, ModernBertModel # type: ignore
+import torch # type: ignore
 
 class Encoder:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        model_name_or_path: Text = "answerdotai/ModernBERT-base"
+    ):
+        self.model_name_or_path = model_name_or_path
+        self.model = self.load_model()
+        self.tokenizer = self.load_tokenizer()
     
-    def encode(self,):
-        pass
+    def encode(
+        self,
+        input_text: Text
+    ):
+        inputs = self.tokenizer(input_text, return_tensors="pt")
+        outputs = self.model(**inputs)
+        return outputs
+    
+    def load_tokenizer(self,):
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path)
+        return tokenizer
+    
+    def load_model(self,):
+        model = ModernBertModel.from_pretrained(self.model_name_or_path)
+        return model
